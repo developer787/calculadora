848bc3da86f1fe8e355cd684d4297b2116205109
ACM Node.js Developer <nodejs787@gmail.com>
2017-01-14 00:39:35 -0400

    first

---
+++ calculadora_magica/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  calculadora_magica
+//
+//  Created by Developer MacBookAir on 1/14/17.
+//  Copyright © 2017 Developer MacBookAir. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
